027b811e3d907ab75afef6061a3f6351b2d4fc33
lena zevina <zevina.l@yandex.ru>
2022-08-31 04:52:19 +0700

    task 8 v2

--- seminar1/hw_task8/Program.cs
+++ seminar1/hw_task8/Program.cs
@@ -1,5 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
 
 Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
