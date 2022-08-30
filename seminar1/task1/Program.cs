4447ea8f153f74d3e0c812be4f56be99f5bfea29
lena zevina <zevina.l@yandex.ru>
2022-08-31 02:10:24 +0700

    task 1, task 3

---
+++ seminar1/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите квадрат числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+  Console.WriteLine("да");
+}
+else
+{
+  Console.WriteLine("нет");
+}
---
+++ seminar1/task3/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+  Console.WriteLine("понедельник");
+}
+else if (number == 2)
+  {
+    Console.WriteLine("вторник");
+  }
+else if (number == 3)
+  {
+    Console.WriteLine("среда");
+  }
+else if (number == 4)
+  {
+    Console.WriteLine("четверг");
+  }
+else if (number == 5)
+  {
+    Console.WriteLine("пятница");
+  }
+else if (number == 6)
+  {
+    Console.WriteLine("суббота");
+  }
+else if (number == 7)
+  {
+    Console.WriteLine("воскресенье");
+  }
