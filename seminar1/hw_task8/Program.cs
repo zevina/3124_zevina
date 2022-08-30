1a61f0d72b56d7cc7134d938d4bec042606cbe6b
lena zevina <zevina.l@yandex.ru>
2022-08-31 03:30:35 +0700

    task 8

---
+++ seminar1/hw_task8/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count >=1 && count <= number)
+{
+  Console.Write(count + ", ");
+  count ++;
+}
+if (number <= 0)
+{
+  Console.WriteLine("число должно быть положительное");
+}
