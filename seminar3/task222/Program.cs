8369f62c3708133e01695ec011530bd77e9fef0f
lena zevina <zevina.l@yandex.ru>
2022-09-07 02:25:57 +0700

    add task 222

--- seminar3/task222/Program.cs
+++ seminar3/task222/Program.cs
@@ -1,4 +1,15 @@
 ﻿// Доп. задание
 // Написать метод для разворота массива, не используя память под дополнительный массив
 
-
+int[] array = new int[] {1, 2, 3, 4, 5};
+int n = array.Length;
+for (int i = 0; i < n/2; i++)
+{
+  int t = array[i];
+  array[i] = array[n-1-i];
+  array[n-1-i] = t;
+}
+foreach (int el in array)
+{
+  Console.Write(el);
+}
