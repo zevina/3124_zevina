f2fca085998a4b5a426f6ea6c20365b2a6ce719a
Zevina Lena <zevina.l@yandex.ru>
2022-10-10 16:48:48 +0700

    change task 51

--- seminar7/task51/Program.cs
+++ seminar7/task51/Program.cs
@@ -61,3 +61,69 @@ Console.WriteLine();
 
 int summa = SumDiagonalMatrix(matr);
 Console.WriteLine($"Cумма элементов, находящихся на главной диагонали: {summa}");
+
+// Решение преподавателя
+
+/*int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 9)
+{
+  int[,] matrix = new int[rowsCount, columnsCount];
+
+  Random rand = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+    }
+  }
+
+  return matrix;
+}
+
+
+int ReadNumber(string message)
+{
+  Console.WriteLine(message);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j]}\t");
+    }
+    Console.WriteLine();
+  }
+}
+int SumMainDiagonal(int[,] matrix)
+{
+  int sum = 0;
+  int min;
+  if (matrix.GetLength(0) >= matrix.GetLength(1))
+  {
+    min = matrix.GetLength(1);
+  }
+  else
+  {
+    min = matrix.GetLength(0);
+  }
+  for (int i = 0; i < min; i++)
+  {
+    sum = sum + matrix[i, i];
+  }
+  return sum;
+}
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+
+System.Console.WriteLine();
+Console.WriteLine("Сумма элементов на диагонали:");
+Console.WriteLine(SumMainDiagonal(matr));*/
